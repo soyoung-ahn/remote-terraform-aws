85768d0b348305972f0fe3c26a0a7acb54d3ead8
daveKim <48761100+salme4@users.noreply.github.com>
2021-06-15 15:58:00 +0900

    mas

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "remote" {
+    hostname = "app.terraform.io"
     organization = "justTest"
-    token = "var.dave_token"
+    token = "${var.dave_token}"
 
     workspaces {
       name = "daveRemoteWorkspace"
