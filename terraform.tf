120929b8a2784922c81557f962b3644c1d41290b
김대현Daehyun kim <daehyun.kim@bespinglobal.com>
2021-06-15 17:30:42 +0900

    rollback

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
     organization = "justTest"
-    token = "${var.dave_token}"
 
     workspaces {
       name = "daveRemoteWorkspace"
