b0b20ab82bc3197518b295a5bd074b0fd76d9044
daveKim <48761100+salme4@users.noreply.github.com>
2021-06-17 09:28:00 +0900

    demo-uplus

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "justTest"
+    organization = "demo-uplus"
 
     workspaces {
-      name = "daveRemoteWorkspace"
+      name = "demo-uplus-workspace"
     }
   }
 }
